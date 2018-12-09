7a87e08352e27710647ebb7480d961bbb26f2f7b
Siteodessa <siteodessa.com@gmail.com>
2018-12-09 03:53:42 +0200

    v1.1.0

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
-
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 
 import { UserList } from './users';
 
 import jsonServerProvider from 'ra-data-json-server';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+import dataProvider from './dataProvider';
 
 
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-        <Resource name="users" list={UserList} />
+    <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
--- src/posts.js
+++ src/posts.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, DisabledInput, SelectInput, TextInput, LongTextInput, ReferenceInput , Create } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, DisabledInput, SelectInput, TextInput, LongTextInput, ReferenceInput , Create , Filter, Responsive, SimpleList} from 'react-admin';
+
+
+
 
 export const PostList = props => (
-    <List {...props}>
-         <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-             <EditButton />
-        </Datagrid>
-    </List>
+  <List {...props}>
+      <Responsive
+          small={
+              <SimpleList
+                  primaryText={record => record.title}
+                  secondaryText={record => `${record.views} views`}
+                  tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+              />
+          }
+          medium={
+              <Datagrid>
+                  <TextField source="id" />
+                  <ReferenceField label="User" source="userId" reference="users">
+                      <TextField source="name" />
+                  </ReferenceField>
+                  <TextField source="title" />
+                  <TextField source="body" />
+                  <EditButton />
+              </Datagrid>
+          }
+      />
+  </List>
 );
 
 
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostEdit = props => (
-    <Edit {...props}>
+
+     <Edit title={<PostTitle />} {...props}>
         <SimpleForm>
           <DisabledInput source="id" />
             <ReferenceInput source="userId" reference="users">
